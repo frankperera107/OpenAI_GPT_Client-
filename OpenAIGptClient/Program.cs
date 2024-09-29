4d0ea0e92d03c8241e90bc4bb9704541877d44c1
Frank Perera <frankperera107@gmail.com>
2024-09-29 08:15:33 +0800

    OpenAI API client

---
+++ OpenAIGptClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+if (args.Length >0)
+{
+    HttpClient client = new();
+
+    //For simplicity using the key directly here. As the best practice better to store it
+    //as a secret and inject it here. 
+    client.DefaultRequestHeaders.Add("authorization", "Bearer <<key>");
+
+
+    var content = new StringContent("{\"model\": \"<<model>>\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+      Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseString);
+
+}
+else
+{
+    Console.WriteLine("---> You need to provide some input.");
+}
+
